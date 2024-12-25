bcc1bd9750d01a16801cc9e572254db353cef218
neodymium <104201402+neodymium6@users.noreply.github.com>
2024-12-26 01:26:44 +0900

    random vs matrix

--- ga/main.py
+++ ga/main.py
@@ -1,25 +1,24 @@
-from rust_reversi import Board, Turn
+from rust_reversi import Arena
+import sys
+
+RANDOM_PLAYER = "players/random_player.py"
+MATRIX_PLAYER = "players/matrix_player.py"
+DEPTH = 4
+
+N_GAMES = 1000
 
 
 def main():
-    board = Board()
+    python = sys.executable
+    random_player = [python, RANDOM_PLAYER]
+    matrix_player = [python, MATRIX_PLAYER, str(DEPTH)]
+    arena = Arena(random_player, matrix_player)
+    arena.play_n(N_GAMES)
 
-    while not board.is_game_over():
-        if board.is_pass():
-            board.do_pass()
-            continue
-
-        move = board.get_random_move()
-
-        board.do_move(move)
-
-    winner = board.get_winner()
-    if winner is None:
-        print("Game drawn.")
-    elif winner == Turn.BLACK:
-        print("Black wins!")
-    else:
-        print("White wins!")
+    p1_win, p2_win, draw = arena.get_stats()
+    print(f"random player wins: {p1_win}")
+    print(f"matrix player wins: {p2_win}")
+    print(f"draw: {draw}")
 
 
 if __name__ == "__main__":
