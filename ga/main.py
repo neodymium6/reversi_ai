62acfae6e52b9b7e8adbd6d3afcd42c8bfdb039e
neodymium <104201402+neodymium6@users.noreply.github.com>
2024-12-26 01:34:28 +0900

    add piece player

--- ga/main.py
+++ ga/main.py
@@ -2,8 +2,11 @@ from rust_reversi import Arena
 import sys
 
 RANDOM_PLAYER = "players/random_player.py"
+PIECE_PLAYER = "players/piece_player.py"
 MATRIX_PLAYER = "players/matrix_player.py"
-DEPTH = 4
+
+PIECE_DEPTH = 4
+MATRIX_DEPTH = 2
 
 N_GAMES = 1000
 
@@ -11,12 +14,13 @@ N_GAMES = 1000
 def main():
     python = sys.executable
     random_player = [python, RANDOM_PLAYER]
-    matrix_player = [python, MATRIX_PLAYER, str(DEPTH)]
-    arena = Arena(random_player, matrix_player)
+    piece_player = [python, PIECE_PLAYER, str(PIECE_DEPTH)]
+    matrix_player = [python, MATRIX_PLAYER, str(MATRIX_DEPTH)]
+    arena = Arena(piece_player, matrix_player)
     arena.play_n(N_GAMES)
 
     p1_win, p2_win, draw = arena.get_stats()
-    print(f"random player wins: {p1_win}")
+    print(f"piece player wins: {p1_win}")
     print(f"matrix player wins: {p2_win}")
     print(f"draw: {draw}")
 
