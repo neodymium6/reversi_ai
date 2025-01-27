87f2e6b57cdf35bcbc47347e7a25695c7891e1f7
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-27 15:49:16 +0900

    sample memory

--- rl/memory/mod.py
+++ rl/memory/mod.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import List, Tuple
 from rust_reversi import Board
 
 class Memory(ABC):
     @abstractmethod
     def push(self, state: Board, action: int, next_state: Board, reward: float) -> None:
+        pass
+
+    def sample(self, batch_size: int) -> List[Tuple[Board, int, Board, float]]:
         pass
