19f315bc5ef98e4700861b74201e1f7e74e6f664
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-27 15:05:29 +0900

    add dropout

--- rl/models/dense.py
+++ rl/models/dense.py
@@ -7,11 +7,14 @@ class DenseNet(torch.nn.Module):
         self.fc2 = torch.nn.Linear(hidden_size, hidden_size)
         self.fc3 = torch.nn.Linear(hidden_size, output_size)
         self.relu = torch.nn.ReLU()
+        self.dropout = torch.nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
