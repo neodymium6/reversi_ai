7b60ccbd86d10e849792850abc5b491c362d37d7
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-27 14:10:31 +0900

    dense model

---
+++ rl/models/dense.py
@@ -0,0 +1,17 @@
+import torch
+
+class DenseNet(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(DenseNet, self).__init__()
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
+        self.fc2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.fc3 = torch.nn.Linear(hidden_size, output_size)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
