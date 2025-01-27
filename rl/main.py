72ddb3c09006f72ebc6328bc492621de09124a6a
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-27 10:48:29 +0900

    use rust-reversi

--- rl/main.py
+++ rl/main.py
@@ -1,5 +1,24 @@
+from rust_reversi import Board, Turn
 def main():
-    print("Hello from rl!")
+    # Start a new game
+    board = Board()
+    while not board.is_game_over():
+        if board.is_pass():
+            board.do_pass()
+            continue
+        # Get random move
+        move = board.get_random_move()
+        # Execute move
+        board.do_move(move)
+
+    # Game over
+    winner = board.get_winner()
+    if winner is None:
+        print("Game drawn.")
+    elif winner == Turn.BLACK:
+        print("Black wins!")
+    else:
+        print("White wins!")
 
 
 if __name__ == "__main__":
