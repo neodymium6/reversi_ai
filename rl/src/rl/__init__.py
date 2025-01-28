16d797fa0ebb011ff38dc7797639e01c2ec69ed3
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-28 23:52:50 +0900

    train_config and vs_config

--- rl/src/rl/__init__.py
+++ rl/src/rl/__init__.py
@@ -3,11 +3,11 @@ from rl.agents.dense import DenseAgent, DenseAgentConfig
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 128
+HIDDEN_SIZE = 256
 
-def train():
-    config = DenseAgentConfig(
+train_config = DenseAgentConfig(
         memory_size=10000,
-        hidden_size=256,
+        hidden_size=HIDDEN_SIZE,
         batch_size=BATCH_SIZE,
         board_batch_size=128,
         device=DEVICE,
@@ -17,50 +17,37 @@ def train():
         lr=1e-4,
         gamma=0.99,
         n_episodes=50000,
-        # n_episodes=1000,
         model_path="dense_agent.pth",
         verbose=True,
-    )
-    agent = DenseAgent(config)
+)
+
+vs_config = DenseAgentConfig(
+    memory_size=10000,
+    hidden_size=HIDDEN_SIZE,
+    batch_size=BATCH_SIZE,
+    board_batch_size=128,
+    device=DEVICE,
+    eps_start=0.0,
+    eps_end=0.0,
+    eps_decay=10,
+    lr=1e-4,
+    gamma=0.99,
+    n_episodes=1000,
+    verbose=False,
+)
+
+def train():
+    agent = DenseAgent(train_config)
     agent.train()
 
-
 def vs_random():
-    config = DenseAgentConfig(
-        memory_size=10000,
-        hidden_size=256,
-        batch_size=BATCH_SIZE,
-        board_batch_size=128,
-        device=DEVICE,
-        eps_start=0.0,
-        eps_end=0.0,
-        eps_decay=10,
-        lr=1e-4,
-        gamma=0.99,
-        n_episodes=1000,
-        verbose=False,
-    )
-    agent = DenseAgent(config)
+    agent = DenseAgent(vs_config)
     agent.load("dense_agent.pth")
     win_rate = agent.vs_random(1000)
     print(f"Win rate: {win_rate}")
 
 def vs_alpha_beta():
-    config = DenseAgentConfig(
-        memory_size=10000,
-        hidden_size=256,
-        batch_size=BATCH_SIZE,
-        board_batch_size=128,
-        device=DEVICE,
-        eps_start=0.0,
-        eps_end=0.0,
-        eps_decay=10,
-        lr=1e-4,
-        gamma=0.99,
-        n_episodes=1000,
-        verbose=True,
-    )
-    agent = DenseAgent(config)
+    agent = DenseAgent(vs_config)
     agent.load("dense_agent.pth")
     win_rate = agent.thunder_vs_alpha_beta(1000)
     print(f"Win rate: {win_rate}")
