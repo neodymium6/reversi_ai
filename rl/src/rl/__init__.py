8cf919e5a012a5166753e79de8eb6ed31b70f1fd
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-29 18:45:19 +0900

    agent as local variable

--- rl/src/rl/__init__.py
+++ rl/src/rl/__init__.py
@@ -31,19 +31,20 @@ vs_config["eps_start"] = 0.0
 vs_config["eps_end"] = 0.0
 vs_config["verbose"] = False
 
-train_agent = CnnAgent(train_config)
-vs_agent = CnnAgent(vs_config)
-
 def train():
+    train_agent = CnnAgent(train_config)
     train_agent.train()
     train_agent.plot()
 
 def vs_random():
+    vs_agent = CnnAgent(vs_config)
     vs_agent.load(vs_agent.config["model_path"])
     win_rate = vs_agent.vs_random(1000)
     print(f"Win rate: {win_rate}")
 
 def vs_alpha_beta():
+    vs_config["verbose"] = True
+    vs_agent = CnnAgent(vs_config)
     vs_agent.load(vs_agent.config["model_path"])
-    win_rate = vs_agent.vs_alpha_beta(1000)
+    win_rate = vs_agent.thunder_vs_alpha_beta(1000)
     print(f"Win rate: {win_rate}")
