e29ee7ee327d040447ac105cbe75f852c2b102dd
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-01-29 12:40:08 +0900

    trin cnn5

--- rl/src/rl/__init__.py
+++ rl/src/rl/__init__.py
@@ -1,26 +1,27 @@
 import copy
 import torch
-from rl.agents.dense import DenseAgent, DenseAgentConfig
+from rl.agents.cnn import CnnAgent, CnnAgentConfig
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-BATCH_SIZE = 128
-HIDDEN_SIZE = 256
+BATCH_SIZE = 256
 
-train_config = DenseAgentConfig(
-        memory_size=int(1e5),
-        hidden_size=HIDDEN_SIZE,
+train_config = CnnAgentConfig(
+        memory_size=int(5e4),
         batch_size=BATCH_SIZE,
         board_batch_size=256,
         device=DEVICE,
         eps_start=0.9,
         eps_end=0.05,
         eps_decay=10,
-        lr=1e-4,
+        lr=5e-4,
         gamma=0.99,
         n_episodes=160000,
         episodes_per_optimize=16,
-        model_path="dense_agent.pth",
         verbose=True,
+
+        num_channels=128,
+        fc_hidden_size=128,
+        model_path="cnn_agent.pth",
 )
 
 vs_config = copy.deepcopy(train_config)
@@ -28,18 +29,18 @@ vs_config["eps_start"] = 0.0
 vs_config["eps_end"] = 0.0
 vs_config["verbose"] = False
 
+train_agent = CnnAgent(train_config)
+vs_agent = CnnAgent(vs_config)
+
 def train():
-    agent = DenseAgent(train_config)
-    agent.train()
+    train_agent.train()
 
 def vs_random():
-    agent = DenseAgent(vs_config)
-    agent.load("dense_agent.pth")
-    win_rate = agent.vs_random(1000)
+    vs_agent.load(vs_agent.config["model_path"])
+    win_rate = vs_agent.vs_random(1000)
     print(f"Win rate: {win_rate}")
 
 def vs_alpha_beta():
-    agent = DenseAgent(vs_config)
-    agent.load("dense_agent.pth")
-    win_rate = agent.thunder_vs_alpha_beta(1000)
+    vs_agent.load(vs_agent.config["model_path"])
+    win_rate = vs_agent.vs_alpha_beta(1000)
     print(f"Win rate: {win_rate}")
