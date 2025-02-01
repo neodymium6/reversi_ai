9e0c0d9cdb159b255661f7460a3eee42902b0bcc
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-02-01 15:26:11 +0900

    per

--- rl/src/rl/memory/proportional.py
+++ rl/src/rl/memory/proportional.py
@@ -15,6 +15,7 @@ class SumTree:
         self.tree = np.zeros(2 * capacity - 1)
         self.length = 0
         self.pointer = 0
+        self.max_p = 0.0
 
     def add(self, priority: float) -> None:
         # if the memory is full, replace the oldest memory
@@ -23,6 +24,8 @@ class SumTree:
         self.length = min(self.length + 1, self.capacity)
 
     def update(self, idx: int, priority: float) -> None:
+        # update max priority
+        self.max_p = max(priority, self.max_p)
         # idx to tree index
         idx = idx + self.capacity - 1
         change = priority - self.tree[idx]
@@ -34,10 +37,9 @@ class SumTree:
             self.tree[idx] += change
             idx = (idx - 1) // 2
 
-    def sample(self, batch_size: int) -> Tuple[List[int], List[float]]:
+    def sample_indices(self, batch_size: int) -> List[int]:
         indices = []
-        priorities = []
-        for i in range(batch_size):
+        for _ in range(batch_size):
             rand = np.random.uniform(0, self.tree[0])
             idx = 0
             while True:
@@ -52,52 +54,40 @@ class SumTree:
                     idx = right
             # idx to memory index
             indices.append(idx - self.capacity + 1)
-            priorities.append(self.tree[idx])
-        return indices, priorities
+        return indices
     
     def sum(self) -> float:
         return self.tree[0]
     
     def __len__(self) -> int:
         return self.length
+    
+    def max(self) -> float:
+        return self.max_p
 
 class ProportionalMemory(Memory):
-    def __init__(self, maxlen):
+    def __init__(self, maxlen: int):
+        is_power_of_two = lambda n: (n != 0) and (n & (n - 1) == 0)
+        if not is_power_of_two(maxlen):
+            maxlen = 2 ** (maxlen.bit_length())
+            print(f"Warning: maxlen is not a power of 2, rounding up to the next power of 2: {maxlen}")
         self.capacity = maxlen
-        self.memory = []
-        self.priorities = []
-        self.max_priority = 1.0
-        self.position = 0
+        self.tree = SumTree(maxlen)
+        self.memory = [None] * maxlen
 
     def push(self, state: Board, action: int, next_state: Board, reward: float) -> None:
-        max_priority = max(self.priorities) if self.priorities else self.max_priority
-
-        if len(self.memory) < self.capacity:
-            self.memory.append((state, action, next_state, reward))
-            self.priorities.append(max_priority)
-        else:
-            # if memory is full, replace the oldest memory
-            self.memory[self.position] = (state, action, next_state, reward)
-            self.priorities[self.position] = max_priority
-        
-        self.position = (self.position + 1) % self.capacity
+        max_priority = self.tree.max()
+        self.memory[self.tree.pointer] = (state, action, next_state, reward)
+        self.tree.add(max_priority)
 
     def sample(self, batch_size) -> Tuple[List[Tuple[Board, int, Board, float]], List[int]]:
-        if len(self.memory) < batch_size:
-            indices = list(range(len(self.memory)))
-            return self.memory, indices
-        
-        properties = np.array(self.priorities)
-        probs = properties / np.sum(properties)
-
-        indices = np.random.choice(len(self.memory), batch_size, p=probs, replace=False)
-
-        return [self.memory[i] for i in indices], indices.tolist()
+        indices = self.tree.sample_indices(batch_size)
+        return [self.memory[i] for i in indices], indices
     
     def update_priorities(self, indices: List[int], priorities: List[float]) -> None:
+        priorities = np.array(priorities) + EPSILON
         for i, priority in zip(indices, priorities):
-            self.priorities[i] = max(priority, EPSILON)
-            self.max_priority = max(self.max_priority, self.priorities[i])
+            self.tree.update(i, priority)
 
     def __len__(self) -> int:
-        return len(self.memory)
+        return len(self.tree)
