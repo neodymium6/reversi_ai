446d4ffbf0e632ebad1114f0c7f7762833ccf192
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-02-05 08:14:49 +0900

    get WTHOR data

--- gen-kifu/src/gen_kifu/__init__.py
+++ gen-kifu/src/gen_kifu/__init__.py
@@ -1,2 +1,26 @@
+import os
+import pathlib
+import time
+
+WTHOR_PATH = "wthor"
+
 def main() -> None:
     print("Hello from gen-kifu!")
+
+def get_wthor() -> None:
+    dir_path = pathlib.Path(WTHOR_PATH)
+    if not dir_path.exists():
+        os.mkdir(WTHOR_PATH)
+    try:
+        for year in range(1990, 2025):
+            if 0 != os.system(f"wget https://www.ffothello.org/wthor/base_zip/WTH_{year}.ZIP"):
+                raise Exception("Failed to download WTHOR files.")
+            if 0 != os.system(f"unzip WTH_{year}.ZIP -d {dir_path / str(year)}"):
+                raise Exception("Failed to unzip WTHOR files.")
+            if 0 != os.system(f"rm WTH_{year}.ZIP"):
+                raise Exception("Failed to remove WTHOR zip files.")
+            time.sleep(1)
+    except Exception as e:
+        print(e)
+        return
+    print("Downloaded WTHOR files.")
