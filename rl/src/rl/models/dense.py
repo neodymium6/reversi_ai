4b41bbd4fd48e9dcccb47da1ec37d7f3c207d67a
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-02-05 18:48:38 +0900

    dense L2

--- rl/src/rl/models/dense.py
+++ rl/src/rl/models/dense.py
@@ -8,14 +8,10 @@ class DenseNet(torch.nn.Module):
     def __init__(self, hidden_size: int):
         super(DenseNet, self).__init__()
         self.fc1 = torch.nn.Linear(INPUT_SIZE, hidden_size)
-        self.fc2 = torch.nn.Linear(hidden_size, hidden_size)
-        self.fc3 = torch.nn.Linear(hidden_size, OUTPUT_SIZE)
-        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(hidden_size, OUTPUT_SIZE)
 
     def forward(self, x) -> torch.Tensor:
         x = self.fc1(x)
-        x = self.relu(x)
+        x = torch.nn.functional.relu(x)
         x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
         return x
