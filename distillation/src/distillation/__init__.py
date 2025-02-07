0b25d5b1571f651758135f41b90dadf98c7219fe
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-02-07 16:09:36 +0900

    distillation baseline

--- distillation/src/distillation/__init__.py
+++ distillation/src/distillation/__init__.py
@@ -1,15 +1,293 @@
+import random
 import numpy as np
 import h5py
+import torch
+from distillation.models.transfomer import Transformer
+from distillation.models.dense import DenseNet
+from sklearn.model_selection import train_test_split
+from rust_reversi import AlphaBetaSearch, Board, MatrixEvaluator, Turn, MctsSearch
+import tqdm
 
-DATA_PATH = "data/kifu.h5"
+MCTS_DATA_PATH = "data/mcts_boards.h5"
+WTHOR_DATA_PATH = "data/wthor_boards.h5"
+TEACHER_MODEL_PATH = "models/teacher_model.pth"
+STUDENT_MODEL_PATH = "models/student_model.pth"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+BATCH_SIZE = 1024
+LR = 1e-4
+N_EPOCHS = 10
+MAX_DATA = int(2e6)
+
+class DistillationDataset(torch.utils.data.Dataset):
+    def __init__(self, X: np.ndarray):
+        self.X = X
+
+    def __len__(self):
+        return len(self.X)
+    
+    def _x2teacher_input(self, x: np.void) -> torch.Tensor:
+        player_board = x["player_board"]
+        opponent_board = x["opponent_board"]
+        turn_str = x["turn"]
+        if turn_str == b'Black':
+            turn = Turn.BLACK
+        else:
+            turn = Turn.WHITE
+        board = Board()
+        board.set_board(player_board, opponent_board, turn)
+        board_matrix = board.get_board_matrix()
+        board_tensor = torch.tensor(board_matrix, dtype=torch.float32)
+        board_tensor = board_tensor[0:2, :, :]
+        return board_tensor
+    
+    def _x2student_input(self, x: np.void) -> torch.Tensor:
+        res = torch.zeros(128, dtype=torch.float32)
+        player_board = x["player_board"]
+        opponent_board = x["opponent_board"]
+        for i in range(64):
+            bit = 1 << (64 - i - 1)
+            if player_board & bit:
+                res[i] = 1.0
+            if opponent_board & bit:
+                res[i + 64] = 1.0
+        return res
+
+    def __getitem__(self, idx):
+        return (self._x2teacher_input(self.X[idx]), self._x2student_input(self.X[idx]))
 
 def load_data() -> np.ndarray:
-    with h5py.File(DATA_PATH, "r") as f:
-        data = f["data"][:]
-        print(f"Loaded data with shape: {data.shape}")
-        print(f"First 5 elements: {data[:5]}")
+    with h5py.File(MCTS_DATA_PATH, "r") as f:
+        mcts_data = f["data"][:]
+        print(f"Loaded MCTS data with shape: {mcts_data.shape}")
+    with h5py.File(WTHOR_DATA_PATH, "r") as f:
+        wthor_data = f["data"][:]
+        print(f"Loaded WTHOR data with shape: {wthor_data.shape}")
+    data: np.ndarray = np.concatenate([mcts_data, wthor_data], axis=0)
+    if data.shape[0] > MAX_DATA:
+        print(f"Trimming data to {MAX_DATA}")
+        data = np.random.choice(data, MAX_DATA, replace=False)
+    return data
+
+def vs_random(n_games: int, net: DenseNet) -> float:
+    print("Vs Random")
+    net.eval()
+    def two_game():
+        win_count = 0
+        # net is black
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if board.get_turn() == Turn.BLACK:
+                action = net.get_action(board)
+            else:
+                action = board.get_random_move()
+            board.do_move(action)
+        if board.is_black_win():
+            win_count += 1
+        # net is white
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if board.get_turn() == Turn.WHITE:
+                action = net.get_action(board)
+            else:
+                action = board.get_random_move()
+            board.do_move(action)
+        if board.is_white_win():
+            win_count += 1
+        return win_count
+
+    win_count = 0
+    for _ in range(n_games // 2):
+        win_count += two_game()
+    win_rate = win_count / n_games
+    return win_rate
+
+def vs_mcts(n_games: int, net: DenseNet) -> float:
+    print("Vs MCTS")
+    net.eval()
+    search = MctsSearch(100, 1.0, 3)
+    def two_game():
+        win_count = 0
+        # net is black
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if board.get_turn() == Turn.BLACK:
+                action = net.get_action(board)
+            else:
+                action = search.get_move(board)
+            board.do_move(action)
+        if board.is_black_win():
+            win_count += 1
+        # net is white
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if board.get_turn() == Turn.WHITE:
+                action = net.get_action(board)
+            else:
+                action = search.get_move(board)
+            board.do_move(action)
+        if board.is_white_win():
+            win_count += 1
+        return win_count
+
+    win_count = 0
+    for _ in range(n_games // 2):
+        win_count += two_game()
+    win_rate = win_count / n_games
+    return win_rate
+
+def vs_alpha_beta(n_games: int, net: DenseNet, epsilon: float = 0.1) -> float:
+    print("Vs AlphaBeta")
+    net.eval()
+    def two_game():
+        evaluator = MatrixEvaluator([
+            [ 40,   1,  4,  0,  0,  4,   1, 40],
+            [  1, -12, -8, -6, -6, -8, -12,  1],
+            [  4,  -8, -1,  0,  0, -1,  -8,  4],
+            [  0,  -6,  0,  0,  0,  0,  -6,  0],
+            [  0,  -6,  0,  0,  0,  0,  -6,  0],
+            [  4,  -8, -1,  0,  0, -1,  -8,  4],
+            [  1, -12, -8, -6, -6, -8, -12,  1],
+            [ 40,   1,  4,  0,  0,  4,   1, 40],
+        ])
+        search = AlphaBetaSearch(evaluator, 4, 1 << 10)
+        win_count = 0
+        # agent is black
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if random.random() < epsilon:
+                action = board.get_random_move()
+                board.do_move(action)
+                continue
+            if board.get_turn() == Turn.BLACK:
+                action = net.get_action(board)
+            else:
+                action = search.get_move(board)
+            board.do_move(action)
+        if board.is_black_win():
+            win_count += 1
+        # agent is white
+        board = Board()
+        while not board.is_game_over():
+            if board.is_pass():
+                board.do_pass()
+                continue
+            if random.random() < epsilon:
+                action = board.get_random_move()
+                board.do_move(action)
+                continue
+            if board.get_turn() == Turn.WHITE:
+                action = net.get_action(board)
+            else:
+                action = search.get_move(board)
+            board.do_move(action)
+        if board.is_white_win():
+            win_count += 1
+        return win_count
+
+    win_count = 0
+    for _ in range(n_games // 2):
+        win_count += two_game()
+    win_rate = win_count / n_games
+    return win_rate
+
+def train_model(data: np.ndarray) -> None:
+    print("Training model...")
+    # load teacher model
+    teacher_net = Transformer(
+        patch_size=2,
+        embed_dim=160,
+        num_heads=5,
+        num_layers=8,
+        mlp_ratio=4.0,
+        dropout=0.0,
+    )
+    state_dict = torch.load(TEACHER_MODEL_PATH, weights_only=True)
+    new_state_dict = {}
+    for key in state_dict:
+        new_key = key.replace('_orig_mod.', '')
+        new_state_dict[new_key] = state_dict[key]
+    teacher_net.load_state_dict(new_state_dict)
+    teacher_net.eval()
+    teacher_net.to(DEVICE)
+    # initialize student model
+    student_net = DenseNet(hidden_size=128)
+    student_net.to(DEVICE)
+
+    # init data loaders
+    X_train, X_test = train_test_split(data, test_size=0.1, shuffle=True)
+    train_dataset = DistillationDataset(X_train)
+    test_dataset = DistillationDataset(X_test)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+    # init optimizer
+    optimizer = torch.optim.AdamW(student_net.parameters(), lr=LR)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=LR,
+        total_steps=N_EPOCHS * len(train_loader),
+        final_div_factor=100,
+    )
+
+    # init criterion
+    criterion = torch.nn.MSELoss()
+
+    # train loop
+    for epoch in range(N_EPOCHS):
+        student_net.train()
+        pb = tqdm.tqdm(total=len(train_loader))
+        for i, (teacher_input, student_input) in enumerate(train_loader):
+            teacher_input: torch.Tensor = teacher_input.to(DEVICE)
+            student_input: torch.Tensor = student_input.to(DEVICE)
+            optimizer.zero_grad()
+            teacher_output = teacher_net(teacher_input)
+            student_output = student_net(student_input)
+            loss: torch.Tensor = criterion(student_output, teacher_output)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(student_net.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+            pb.update(1)
+        pb.close()
+        student_net.eval()
+        with torch.no_grad():
+            test_loss = 0.0
+            for i, (teacher_input, student_input) in enumerate(test_loader):
+                teacher_input = teacher_input.to(DEVICE)
+                student_input = student_input.to(DEVICE)
+                teacher_output = teacher_net(teacher_input)
+                student_output = student_net(student_input)
+                loss = criterion(student_output, teacher_output)
+                test_loss += loss.item()
+            test_loss /= len(test_loader)
+            print(f"Epoch: {epoch}, Test Loss: {test_loss:.4f}")
+            random_win_rate = vs_random(100, student_net)
+            mcts_win_rate = vs_mcts(100, student_net)
+            alpha_beta_win_rate = vs_alpha_beta(100, student_net)
+            print(f"Random Win Rate: {random_win_rate}, MCTS Win Rate: {mcts_win_rate}, AlphaBeta Win Rate: {alpha_beta_win_rate}")
+        torch.save(student_net.state_dict(), STUDENT_MODEL_PATH)
+
+    random_win_rate = vs_random(1000, student_net)
+    mcts_win_rate = vs_mcts(1000, student_net)
+    alpha_beta_win_rate = vs_alpha_beta(1000, student_net)
+    print(f"Final Random Win Rate: {random_win_rate}, MCTS Win Rate: {mcts_win_rate}, AlphaBeta Win Rate: {alpha_beta_win_rate}")
 
 
 def main() -> None:
-    print("Hello from distillation!")
-    load_data()
+    data = load_data()
+    train_model(data)
--- distillation/src/distillation/models/dense.py
+++ distillation/src/distillation/models/dense.py
@@ -1,8 +1,10 @@
 import torch
+from rust_reversi import Board
 
 INPUT_SIZE = 128
 # 8x8 board + 1 for pass
 OUTPUT_SIZE = 65
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DenseNet(torch.nn.Module):
     def __init__(self, hidden_size: int):
@@ -15,3 +17,23 @@ class DenseNet(torch.nn.Module):
         x = torch.nn.functional.relu(x)
         x = self.fc2(x)
         return x
+
+    def get_action(self, board: Board) -> int:
+        board_tensor = torch.zeros(128, dtype=torch.float32)
+        player_board, opponent_board, _turn = board.get_board()
+        for i in range(64):
+            bit = 1 << (64 - i - 1)
+            if player_board & bit:
+                board_tensor[i] = 1.0
+            if opponent_board & bit:
+                board_tensor[i + 64] = 1.0
+        board_tensor = board_tensor.to(DEVICE)
+        with torch.no_grad():
+            output = self.forward(board_tensor)
+        legal_actions = torch.tensor(
+            board.get_legal_moves_tf() + [board.is_pass()],
+            dtype=torch.bool,
+            device=DEVICE,
+        )
+        output.masked_fill_(~legal_actions, -1e9)
+        return torch.argmax(output).item()
