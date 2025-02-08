6af7dce984bee69f5b16d01dd612ae3a2cb76674
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-02-08 21:33:36 +0900

    remove sign in adv

--- distillation/src/distillation/losses/mse_ranking.py
+++ distillation/src/distillation/losses/mse_ranking.py
@@ -1,22 +1,19 @@
 import torch
 
 class MSEWithRankingLoss(torch.nn.Module):
-    def __init__(self, rank_weight: float = 0.1):
+    def __init__(self, rank_weight: float = 0.1, margin: float = 0.1):
         super(MSEWithRankingLoss, self).__init__()
         self.rank_weight = rank_weight
+        self.margin = margin
         self.mse = torch.nn.MSELoss()
 
     def _compute_ranking_loss(self, student_q: torch.Tensor, teacher_q: torch.Tensor) -> torch.Tensor:
         teacher_advantages = teacher_q.unsqueeze(1) - teacher_q.unsqueeze(2)
         student_advantages = student_q.unsqueeze(1) - student_q.unsqueeze(2)
 
-        # only care about the sign of the advantages
-        teacher_advantages = torch.sign(teacher_advantages)
-        student_advantages = torch.sign(student_advantages)
-
         ranking_loss = torch.mean(
             torch.relu(
-                -(teacher_advantages * student_advantages)
+                -(teacher_advantages * student_advantages) + self.margin
             )
         )
 
