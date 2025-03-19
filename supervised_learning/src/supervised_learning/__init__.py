813d4cee71faf80ee07443f71d77142a175e5d28
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-03-19 22:47:11 +0900

    iterative data loading

--- supervised_learning/src/supervised_learning/__init__.py
+++ supervised_learning/src/supervised_learning/__init__.py
@@ -1,5 +1,5 @@
 import h5py
-from typing import List, Tuple
+from typing import List, Tuple, Iterable
 import tqdm
 import numpy as np
 from supervised_learning.models.dense import DenseNet
@@ -17,68 +17,77 @@ LOSS_PLOT_PATH = "loss.png"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_PATH = "model.pth"
 
-MAX_DATA = int(1e6) * 1
+MAX_DATA = int(1e6) * 200
 BATCH_SIZE = 512
 LR = 1e-3
 WEIGHT_DECAY = 1e-6
 N_EPOCHS = 100
 HIDDEN_SIZE = 64
-PREPROCESS_WORKERS = 16
-NUM_WORKERS = 4
-DATA_LOADER_UPDATE_PER_EPOCH = 20
+PREPROCESS_WORKERS = 20
+DATA_LOADER_WORKERS = 4
+DATA_LOADER_UPDATE_PER_EPOCH = 100
 MODEL_CLASS = DenseNet
 
-def load_data(verbose: bool) -> List[Tuple[int, int, int]]:
+def load_data(verbose: bool) -> Iterable[List[Tuple[int, int, int]]]:
     if verbose:
         print("Loading data...")
-    loaded_data: List[Tuple[int, int, int]] = []
     with h5py.File(DATA_PATH, "r") as f:
         all_data = f["data"][:]
         if all_data.shape[0] > MAX_DATA:
             if verbose:
                 print(f"Data size is too large, truncate to {MAX_DATA}")
                 print(f"Using {MAX_DATA / all_data.shape[0] * 100:.2f}% of data")
-            all_data = np.random.choice(all_data, MAX_DATA, replace=False)
+            data_indices = np.random.choice(all_data.shape[0], MAX_DATA, replace=False)
         else:
             if verbose:
                 print(f"Data size: {all_data.shape[0]}")
-        for data in tqdm.tqdm(all_data, desc="Loading data", leave=False):
+            data_indices = np.arange(all_data.shape[0])
+        chunk_size = int(1e7)
+        loaded_data: List[Tuple[int, int, int]] = []
+        for idx in tqdm.tqdm(data_indices, desc="Loading data", leave=False):
+            data = all_data[idx]
             player_board = data[0]
             opponent_board = data[1]
             score = data[2]
             loaded_data.append((player_board, opponent_board, score))
-    return loaded_data
+            if len(loaded_data) >= chunk_size:
+                yield loaded_data
+                del loaded_data
+                loaded_data = []
+        if len(loaded_data) > 0:
+            yield loaded_data
 
 def get_dataloaders(verbose=True) -> Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, int]:
     start_time = time.time()
-    data = load_data(verbose)
-    data_train, data_test = train_test_split(data, test_size=0.1, shuffle=True)
-    del data
     train_dataset = ReversiDataset(
         model_class=MODEL_CLASS,
         preprocess_workers=PREPROCESS_WORKERS,
-        verbose=verbose,
+        verbose=False,
     )
-    train_dataset.append_data(data_train)
-    del data_train
     test_dataset = ReversiDataset(
         model_class=MODEL_CLASS,
         shuffle=False,
         preprocess_workers=PREPROCESS_WORKERS,
-        verbose=verbose,
+        verbose=False,
     )
-    test_dataset.append_data(data_test)
-    del data_test
+    for chunk in load_data(verbose):
+        data_train, data_test = train_test_split(chunk, test_size=0.1, shuffle=True)
+        del chunk
+        train_dataset.append_data(data_train)
+        del data_train
+        test_dataset.append_data(data_test)
+        del data_test
+
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
-        prefetch_factor=50,
+        num_workers=DATA_LOADER_WORKERS,
+        prefetch_factor=10,
     )
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=BATCH_SIZE,
-        num_workers=NUM_WORKERS,
+        num_workers=DATA_LOADER_WORKERS,
         prefetch_factor=10,
     )
     time_elapsed = time.time() - start_time
