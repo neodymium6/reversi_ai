1bcc316e675337ae5de619ea70c7078b0e9c1c28
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2025-03-19 14:49:59 +0900

    implement trainer class

--- supervised_learning/src/supervised_learning/__init__.py
+++ supervised_learning/src/supervised_learning/__init__.py
@@ -4,13 +4,12 @@ import tqdm
 import numpy as np
 from supervised_learning.models.dense import DenseNet
 import torch
-from supervised_learning.vs import vs_random, vs_mcts, vs_alpha_beta
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 from supervised_learning.reversi_dataset import ReversiDataset
 from torch_optimizer import Lookahead
 from supervised_learning.losses.sign_aware import SignAwareMAE
 import time
+from supervised_learning.reversi_trainer import Trainer
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 DATA_PATH = "egaroucid_augmented.h5"
@@ -26,7 +25,7 @@ N_EPOCHS = 100
 HIDDEN_SIZE = 64
 PREPROCESS_WORKERS = 16
 NUM_WORKERS = 4
-DATA_LOADER_UPDATE_PER_EPOCH = 200
+DATA_LOADER_UPDATE_PER_EPOCH = 20
 MODEL_CLASS = DenseNet
 
 def load_data(verbose: bool) -> List[Tuple[int, int, int]]:
@@ -91,104 +90,30 @@ def main() -> None:
     net = DenseNet(HIDDEN_SIZE)
     net.to(DEVICE)
 
-    train_loader, test_loader, get_dataloader_time = get_dataloaders()
-
     base_optimizer = torch.optim.AdamW(net.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
     optimizer = Lookahead(base_optimizer, k=5, alpha=0.5)
-
-    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer,
-        mode="min",
-        patience=5,
-    )
 
     criterion = SignAwareMAE(
         sign_penalty_weight=0.0,
     )
 
-    train_losses = []
-    test_losses = []
-    lrs = []
-    epoch_pb = tqdm.tqdm(range(N_EPOCHS))
-    for epoch in epoch_pb:
-        net.train()
-        total_loss = 0.0
-        train_pb = tqdm.tqdm(train_loader, desc=f"Epoch {epoch}", leave=False)
-        for inputs, targets in train_pb:
-            inputs: torch.Tensor
-            targets: torch.Tensor
-            inputs = inputs.to(DEVICE)
-            targets = targets.to(DEVICE)
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss: torch.Tensor = criterion(outputs.view(-1), targets)
-            total_loss += loss.item()
-            train_pb.set_description(f"Epoch {epoch} - Loss: {loss.item():5.2f}")
-            torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
-            loss.backward()
-            optimizer.step()
-        train_losses.append(total_loss / len(train_loader))
-        net.eval()
-        with torch.no_grad():
-            total_loss = 0.0
-            for inputs, targets in test_loader:
-                inputs: torch.Tensor
-                targets: torch.Tensor
-                inputs = inputs.to(DEVICE)
-                targets = targets.to(DEVICE)
-                outputs: torch.Tensor = net(inputs)
-                loss = criterion(outputs.view(-1), targets)
-                total_loss += loss.item()
-            test_losses.append(total_loss / len(test_loader))
-            lr_scheduler.step(total_loss)
-            lrs.append(lr_scheduler.get_last_lr()[0])
-            plot_loss(epoch, train_losses, test_losses, lrs)
-
-        if epoch % (N_EPOCHS // 10) == 0:
-            torch.save(net.state_dict(), MODEL_PATH)
-            n_games = 100
-            random_win_rate = vs_random(n_games, net)
-            mcts_win_rate = vs_mcts(n_games, net)
-            alpha_beta_win_rate = vs_alpha_beta(n_games, net)
-            epoch_pb.write(f"Epoch {epoch:{len(str(N_EPOCHS))}d}: Win rate vs random: {random_win_rate:.4f}, vs MCTS: {mcts_win_rate:.4f}, vs alpha beta: {alpha_beta_win_rate:.4f}")
-
-        if epoch % DATA_LOADER_UPDATE_PER_EPOCH == DATA_LOADER_UPDATE_PER_EPOCH - 1 and epoch != N_EPOCHS - 1:
-            start_time = time.time()
-            end_time_pred = time.localtime(start_time + get_dataloader_time)
-            end_time_pred = time.strftime("%H:%M:%S", end_time_pred)
-            epoch_pb.write(f"Updating data loaders... Expected to finish at {end_time_pred}")
-            train_loader, test_loader, get_dataloader_time = get_dataloaders(verbose=False)
-    torch.save(net.state_dict(), MODEL_PATH)
-    plot_loss(N_EPOCHS, train_losses, test_losses, lrs)
-
-    n_games = 500
-    random_win_rate = vs_random(n_games, net)
-    mcts_win_rate = vs_mcts(n_games, net)
-    alpha_beta_win_rate = vs_alpha_beta(n_games, net)
-    print(f"Epoch {N_EPOCHS:{len(str(N_EPOCHS))}d}: Win rate vs random: {random_win_rate:.4f}, vs MCTS: {mcts_win_rate:.4f}, vs alpha beta: {alpha_beta_win_rate:.4f}")
-
-def plot_loss(epoch: int, train_losses: List[float], test_losses: List[float], lrs: List[float]) -> None:
-    fig, ax1 = plt.subplots(figsize=(10, 6))
-    color1 = 'blue'
-    color2 = 'orange'
-    ax1.plot(train_losses, label="train", color=color1)
-    ax1.plot(test_losses, label="test", color=color2)
-    ax1.set_xlabel("Epoch")
-    ax1.set_ylabel("Loss")
-    ax1.set_yscale("log")
-    ax1.legend(loc='upper left')
-
-    ax2 = ax1.twinx()
-    color3 = 'red'
-    ax2.plot(lrs, label="learning rate", color=color3, linestyle='--')
-    ax2.set_ylabel("Learning Rate")
-    ax2.set_yscale("log")
-    ax2.legend(loc='upper right')
-    
-    plt.title(f"Training Progress - Epoch {epoch}")
-    plt.tight_layout()
-    plt.savefig(LOSS_PLOT_PATH)
-    plt.close(fig)
+    trainer = Trainer(
+        net=net,
+        get_data_loader_func=get_dataloaders,
+        optimizer=optimizer,
+        criterion=criterion,
+        device=DEVICE,
+        loss_plot_path=LOSS_PLOT_PATH,
+        model_path=MODEL_PATH,
+        middle_n_games=100,
+        final_n_games=500,
+        data_loader_update_per_epoch=DATA_LOADER_UPDATE_PER_EPOCH,
+        verbose=True,
+    )
+    cnt = 0
+    for _ in trainer.train(N_EPOCHS):
+        cnt += 1
+    print(f"Training completed in {cnt} epochs")
 
 def export():
     net = DenseNet(HIDDEN_SIZE)
---
+++ supervised_learning/src/supervised_learning/reversi_trainer.py
@@ -0,0 +1,128 @@
+import torch
+from typing import Iterable, Callable, Tuple
+import tqdm
+from supervised_learning.models import ReversiNet
+import matplotlib.pyplot as plt
+from supervised_learning.vs import vs_random, vs_mcts, vs_alpha_beta
+import time
+
+class Trainer:
+    def __init__(
+            self,
+            net: ReversiNet,
+            get_data_loader_func: Callable[[bool], Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, int]],
+            optimizer: torch.optim.Optimizer,
+            criterion: torch.nn.Module,
+            device: torch.device,
+            loss_plot_path: str,
+            model_path: str,
+            middle_n_games: int = 100,
+            final_n_games: int = 500,
+            data_loader_update_per_epoch: int = 10,
+            verbose: bool = True,
+        ):
+        self.net = net
+        self.train_loader, self.test_loader, self.get_dataloader_time = get_data_loader_func(verbose)
+        self.get_data_loader_func = get_data_loader_func
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        self.loss_plot_path = loss_plot_path
+        self.model_path = model_path
+        self.middle_n_games = middle_n_games
+        self.final_n_games = final_n_games
+        self.data_loader_update_per_epoch = data_loader_update_per_epoch
+        self.verbose = verbose
+        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            self.optimizer,
+            mode="min",
+            patience=5,
+        )
+        self.train_losses = []
+        self.test_losses = []
+        self.lrs = []
+
+    def plot_loss(self, epoch: int) -> None:
+        fig, ax1 = plt.subplots(figsize=(10, 6))
+        color1 = 'blue'
+        color2 = 'orange'
+        ax1.plot(self.train_losses, label="train", color=color1)
+        ax1.plot(self.test_losses, label="test", color=color2)
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("Loss")
+        ax1.set_yscale("log")
+        ax1.legend(loc='upper left')
+
+        ax2 = ax1.twinx()
+        color3 = 'red'
+        ax2.plot(self.lrs, label="learning rate", color=color3, linestyle='--')
+        ax2.set_ylabel("Learning Rate")
+        ax2.set_yscale("log")
+        ax2.legend(loc='upper right')
+        
+        plt.title(f"Training Progress - Epoch {epoch}")
+        plt.tight_layout()
+        plt.savefig(self.loss_plot_path)
+        plt.close(fig)
+
+    def eval_with_win_rate(self, n_games) -> str:
+        random_win_rate = vs_random(n_games, self.net)
+        mcts_win_rate = vs_mcts(n_games, self.net)
+        alpha_beta_win_rate = vs_alpha_beta(n_games, self.net)
+        return f"Win rate vs random: {random_win_rate:.4f}, vs MCTS: {mcts_win_rate:.4f}, vs alpha beta: {alpha_beta_win_rate:.4f}"
+
+    def train(self, epochs: int) -> Iterable[None]:
+        epoch_pb = tqdm.tqdm(range(epochs), leave=False)
+        for epoch in epoch_pb:
+            self.net.train()
+            total_loss = 0.0
+            train_pb = tqdm.tqdm(self.train_loader, desc=f"Epoch {epoch}", leave=False)
+            for inputs, targets in train_pb:
+                inputs: torch.Tensor
+                targets: torch.Tensor
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device)
+                self.optimizer.zero_grad()
+                outputs: torch.Tensor = self.net(inputs)
+                loss: torch.Tensor = self.criterion(outputs.view(-1), targets)
+                total_loss += loss.item()
+                train_pb.set_description(f"Epoch {epoch} - Loss: {loss.item():5.2f}")
+                torch.nn.utils.clip_grad_norm_(self.net.parameters(), 1.0)
+                loss.backward()
+                self.optimizer.step()
+            self.train_losses.append(total_loss / len(self.train_loader))
+            self.net.eval()
+            with torch.no_grad():
+                total_loss = 0.0
+                test_pb = tqdm.tqdm(self.test_loader, desc=f"Epoch {epoch} - Test", leave=False)
+                for inputs, targets in test_pb:
+                    inputs: torch.Tensor
+                    targets: torch.Tensor
+                    inputs = inputs.to(self.device)
+                    targets = targets.to(self.device)
+                    outputs: torch.Tensor = self.net(inputs)
+                    loss = self.criterion(outputs.view(-1), targets)
+                    total_loss += loss.item()
+                    test_pb.set_description(f"Epoch {epoch} - Test Loss: {loss.item():5.2f}")
+                self.test_losses.append(total_loss / len(self.test_loader))
+                self.lr_scheduler.step(total_loss)
+                self.lrs.append(self.lr_scheduler.get_last_lr()[0])
+            self.plot_loss(epoch)
+
+            if epoch % (epochs // 10) == 0:
+                torch.save(self.net.state_dict(), self.model_path)
+                if self.verbose:
+                    epoch_pb.write(f"Epoch {epoch:{len(str(epochs))}d}: {self.eval_with_win_rate(self.middle_n_games)}")
+
+            if epoch % self.data_loader_update_per_epoch == self.data_loader_update_per_epoch - 1 and epoch != epochs - 1:
+                start_time = time.time()
+                end_time_pred = time.localtime(start_time + self.get_dataloader_time)
+                end_time_pred = time.strftime("%H:%M:%S", end_time_pred)
+                if self.verbose:
+                    epoch_pb.write(f"Updating data loaders... Expected to finish at {end_time_pred}")
+                self.train_loader, self.test_loader, self.get_dataloader_time = self.get_data_loader_func(False)
+            yield
+        torch.save(self.net.state_dict(), self.model_path)
+        self.plot_loss(epochs)
+        if self.verbose:
+            epoch_pb.write(f"Training completed. {self.eval_with_win_rate(self.final_n_games)}")
